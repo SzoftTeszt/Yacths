fa9ba5a6f0fa318acbc64f96d84ba7bec7976da2
Erős István <eros@zold.lan>
2025-05-28 20:07:17 +0200

    05.28

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -5,7 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(yacths:any,mezo:any,irany:any): any {
+    if (!yacths) return []
+    if (!mezo || mezo=="") return yacths;
+    return yacths.sort(
+      (elso:any,masodik:any)=>{
+        let a = elso[mezo]
+        let b = masodik[mezo]
+        
+        if  (typeof a=='string')
+          return irany=='asc'?
+        a.localeCompare(b,'hu',{ sensitivity: "base" })
+        :b.localeCompare(a,'hu',{ sensitivity: "base" })
+
+        if (a<b) return irany=='asc'?-1:1
+        if (a>b) return irany=='asc'?1:-1
+        return 0;
+
+      }
+    )
+
     return null;
   }
 
--- src/app/yachts-list/yachts-list.component.ts
+++ src/app/yachts-list/yachts-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BaseService } from '../base.service';
+import { NgbCalendar, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-yachts-list',
@@ -8,18 +9,82 @@ import { BaseService } from '../base.service';
 })
 export class YachtsListComponent {
   yacths:any
+  calendar = inject(NgbCalendar);
+
+	hoveredDate: NgbDate | null = null;
+	fromDate: NgbDate = this.calendar.getNext(this.calendar.getToday(),'d',1);
+	toDate: NgbDate | null = this.calendar.getNext(this.fromDate, 'd', 3);
+
+  searchField=""
+  lehetosegeg=[
+    {text:"Nincs", key:""},
+    {text:"Név", key:"name"},
+    {text:"Gyártó", key:"builder"},
+    {text:"Ár", key:"daily_price"},
+    {text:"Foglalo", key:"deposit"},
+  ]
+
+
   constructor(private base:BaseService){
     this.base.getYacts().subscribe(
       (res)=>this.yacths=res
     )
   }
 
+	onDateSelection(date: NgbDate) {
+		if (!this.fromDate && !this.toDate) {
+			this.fromDate = date;
+		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+			this.toDate = date;
+		} else {
+			this.toDate = null;
+			this.fromDate = date;
+		}
+	}
+
+	isHovered(date: NgbDate) {
+		return (
+			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+		);
+	}
+
+	isInside(date: NgbDate) {
+		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+	}
+
+	isRange(date: NgbDate) {
+		return (
+			date.equals(this.fromDate) ||
+			(this.toDate && date.equals(this.toDate)) ||
+			this.isInside(date) ||
+			this.isHovered(date)
+		);
+	}
+  ngbDateToString(ngbDate:NgbDateStruct){
+    const year = ngbDate.year;
+    const month = String(ngbDate.month).padStart(2,'0')
+    const day = String(ngbDate.day).padStart(2,'0')
+    const s= `${year}-${month}-${day}`
+    console.log(s)
+    return s;
+  }
+
+
+
   foglalas(yacth:any){
     const body = Object.assign({},yacth)
     console.log(body)
     delete body.image
     
-    const body2 = {yacthId:yacth.id, deposit:yacth.deposit, daily_price:yacth.daily_price, uid:"Attila"}
+    const body2 = {
+      yacthId:yacth.id, 
+      deposit:yacth.deposit, 
+      daily_price:yacth.daily_price, 
+      uid:"Attila",
+      startDate: this.ngbDateToString(this.fromDate),
+      endDate: this.toDate?this.ngbDateToString(this.toDate):null,
+      yacthName: yacth.name
+    }
     this.base.postYact(body2)
   }
 
